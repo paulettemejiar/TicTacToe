13eba267fe7955dd315a2b47295856fe33242b4b
paulette <paulette@localhost>
2023-04-27 00:55:21 -0400

    my tic tac toe game :)

---
+++ src/tic/tac/toe/TicTacToe.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tic.tac.toe;
+
+/**
+ *
+ * @author paulette
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       TicTacToeBoard board= new TicTacToeBoard();
+               board.setVisible(true);
+               
+               }
+    
+}
